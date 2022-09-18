0f5a953f33d89da59e721acd253482ab1258726a
Maia Dutta <mermaia33@gmail.com>
2022-09-18 11:17:45 -0500

    added change events to JewelryStyles, DiamondSizes, and Metals

--- scripts/DiamondSizes.js
+++ scripts/DiamondSizes.js
@@ -6,7 +6,7 @@ document.addEventListener(
     "change",
     (event) => {
         if (event.target.name === "size") {
-            window.alert(``)
+            window.alert(`User chose size ${event.target.value}`)
         }
     }
 )
@@ -15,13 +15,15 @@ export const DiamondSizes = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
+    // The map() array method is a conversion tool. It generates a new array with as many items as are in the original array, but in the new array, it puts items ***in the form that you specify***
+
     const listItems = sizes.map(size => {
         return `<li>
-            <input type="radio" name="size" value="${size.id}" /> ${size.carets}
+            <input type="radio" name="size" value="${size.id}" /> ${size.carats}
         </li>`
     })
-
-    html += listItems.join("")
+    // So an object comes into your function, and a string gets returned. That string goes into a new array. Because .map() produces a new array with the items items of the original array transformed based on our instructions(the function we gave to map), we need to use .join() to join the individual strings together b/c we don't want an array
+     html += listItems.join("")//joins strings in the array list items together. In this case, the separator (i.e what goes inside the parenthesis) is an empty string, which means there will be nothing in between the list items. You can put anything inside the (), and that will appear between the list items
     html += "</ul>"
 
     return html
--- scripts/JewelryStyles.js
+++ scripts/JewelryStyles.js
@@ -5,6 +5,9 @@ const styles = getStyles()
 document.addEventListener(
     "change",
     (event) => {
+        if (event.target.name === "style") {
+            window.alert(`User chose style ${event.target.value}`)
+        }
     }
 )
 
@@ -12,11 +15,15 @@ export const JewelryStyles = () => {
     let html = "<ul>"
 
     // Use .map() for converting objects to <li> elements
-    const listItemsArray =
-
+    const listItemsArray = styles.map(style =>{
+        return `<li>
+        <input type="radio" name="style" value="${style.id}" />
+        ${style.style}
+        </li>`
+    })
 
     // Join all of the strings in the array into a single string
-    html += listItems.join("")
+    html += listItemsArray.join("")
 
     html += "</ul>"
     return html
